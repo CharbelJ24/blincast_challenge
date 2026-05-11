25a5e9480cb0912186de4784e293fbac92e44cdd
Charbel Janos Freitas <charbeljanos@tecgraf.puc-rio.br>
2026-05-11 20:52:11 -0300

    cria metodo update e get para testes

---
+++ database.js
@@ -0,0 +1,31 @@
+export class DatabaseMemory {
+    #data = new Map()
+
+    list() {
+        return Array.from(this.#data.entries()).map(([key, value]) => {
+            return { key, value }
+        })
+    }
+
+    create(key, value) {
+        //  key ja existe
+        if(this.#data.has(key)){
+            return { status: 'error', code: 1 }
+        }
+        
+        // key nao existe 
+        this.#data.set(key, value)
+        return { status: 'ok', code: 0 }
+    }
+
+    update(key, value) {
+        // key não existe
+        if (!this.#data.has(key)) {
+            return { status: 'error', code: -1}
+        }
+
+        // key existe
+        this.#data.set(key, value)
+        return { status: 'ok', code: 0 }
+    }
+}
--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database.js'
 
-const server = fastify({ logger: true })
+const server = fastify()
 
-const database = { /* nome: "", idade: "" */}
+const database = new DatabaseMemory()
+
+server.get('/document', (request, reply) => {
+    const result = database.list()
+    return reply.send(result)
+})
 
 server.post('/document', (request, reply) => {
     const { action, key, value } = request.body
 
     if (action == 'create') {
-        //  key ja existe
-        if(database[key] !== undefined){
-            return reply.status(409).send({ status: 'error', code: 6 })
-        }
-        
-        // key nao existe 
-        database[key] = value
-        return reply.send({ status: 'ok', code: 0 })
+        const result = database.create(key, value)
+        return reply.status(result.code === 0 ? 200: 409).send(result)
     }
 
-    // if (action == 'update') {
-
-    // }
+    if (action == 'update') {
+        const result = database.update(key, value)
+        return reply.status(result.code === 0 ? 200: 404).send(result)
+    }
 
     // if (action == 'delete') {
 
