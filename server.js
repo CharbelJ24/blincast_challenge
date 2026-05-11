0e637dcff0e38c4361d699c124b30f25ee5aeabc
Charbel Janos Freitas <charbeljanos@tecgraf.puc-rio.br>
2026-05-11 19:27:14 -0300

    cria e testa action create

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 import { fastify } from 'fastify'
 
-const server = fastify()
+const server = fastify({ logger: true })
+
+const database = { /* nome: "", idade: "" */}
+
+server.post('/document', (request, reply) => {
+    const { action, key, value } = request.body
+
+    if (action == 'create') {
+        //  key ja existe
+        if(database[key] !== undefined){
+            return reply.status(409).send({ status: 'error', code: 6 })
+        }
+        
+        // key nao existe 
+        database[key] = value
+        return reply.send({ status: 'ok', code: 0 })
+    }
+
+    // if (action == 'update') {
+
+    // }
+
+    // if (action == 'delete') {
+
+    // }
+})
 
 server.listen({
     port: 3333,
